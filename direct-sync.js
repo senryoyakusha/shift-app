4abbd22469d2abbe617e3f39398383e1adf1ff1e
senryoyakusha <office@senryoyakusha.com>
2026-09-04 10:20:47 +0900

    fix: mirror Shift direct bootstrap v2026-09-04.3

--- direct-sync.js
+++ direct-sync.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
 
-  var BOOTSTRAP_VERSION = "2026-09-04.2";
+  var BOOTSTRAP_VERSION = "2026-09-04.3";
   var scripts = [
     "./direct-sync-core.js?v=" + encodeURIComponent(BOOTSTRAP_VERSION),
     "./direct-recovery.js?v=" + encodeURIComponent(BOOTSTRAP_VERSION)
@@ -19,11 +19,5 @@
     (document.head || document.documentElement).appendChild(script);
   }
 
-  if (document.readyState === "loading") {
-    scripts.forEach(function (src) {
-      document.write('<script src="' + src + '"><\\/script>');
-    });
-  } else {
-    loadSequentially(0);
-  }
+  loadSequentially(0);
 })();
